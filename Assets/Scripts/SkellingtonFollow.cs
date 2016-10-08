6ddf145b160e984933cb9fdcdd5c82a9473a979c
Allen Brower <bslashingu@gmail.com>
2016-10-08 18:24:29 -0400

    Added in the bouncing skulls and ads, among other things

---
+++ Assets/Scripts/SkellingtonFollow.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkellingtonFollow : MonoBehaviour {
+
+	public float Speed = 5.0f;
+	public float RandomRange = .5f;
+
+	private Vector3 _randomoffset;
+
+	private float _timeSinceLastChange = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+
+		_randomoffset = new Vector3(Random.Range(-.5f, .5f), -.75f, Random.Range(-.5f, .5f));
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = Vector3.Lerp(transform.position, Camera.main.transform.position + _randomoffset, Speed * Time.deltaTime);
+
+		if(Time.time >= _timeSinceLastChange + 5.0f){
+			_randomoffset = new Vector3(Random.Range(-RandomRange, RandomRange), -.75f, Random.Range(-RandomRange, RandomRange));
+			_timeSinceLastChange = Time.time;
+		}
+	
+	}
+}
